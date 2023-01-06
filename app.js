58cfbbdc6914bc2bc9d3c4b4285c40dc20c1b0ea
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-01-06 12:40:49 -0500

    Change color of button

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const app = Vue.createApp({
     data() {
-        return {};
+        return { buttonChangeClasses: "" };
     },
     methods: {
-        changeBox(id) {},
+        changeButtonStyle() {
+            if (this.buttonChangeClasses === "") {
+                this.buttonChangeClasses = "change1";
+            } else if (this.buttonChangeClasses === "change1") {
+                this.buttonChangeClasses = "change2";
+            } else {
+                this.buttonChangeClasses = "";
+            }
+            console.log(this.buttonChangeClasses);
+        },
     },
-    computed: {},
 });
 
 app.mount("#styling");
