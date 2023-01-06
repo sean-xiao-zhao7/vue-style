a9b0c5df3e277f90221fed30a3d159319c49a042
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-01-06 15:50:51 -0500

    Add attack logics

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
+const randomAttack = (baseValue, extraValue) => {
+    return Math.floor(Math.random() * extraValue) + baseValue;
+};
+
 const app = Vue.createApp({
     data() {
-        return {};
+        return {
+            monsterHealth: 100,
+            playerHealth: 100,
+            monsterAttack: 50,
+            playerAttack: 10,
+            monsterAttackExtra: 10,
+            playerAttackExtra: 10,
+            playerSpecialAttackExtra: 50,
+        };
     },
     methods: {
-        attack() {},
-        specialAttack() {},
+        attack() {
+            this.monsterHealth -= randomAttack(
+                this.playerAttack,
+                playerAttackExtra
+            );
+            if (this.monsterHealth < 0) {
+                this.monsterHealth = 0;
+            }
+        },
+        specialAttack() {
+            this.monsterHealth -= randomAttack(
+                this.playerAttack,
+                playerSpecialAttackExtra
+            );
+            if (this.monsterHealth < 0) {
+                this.monsterHealth = 0;
+            }
+        },
         heal() {},
         skip() {},
+        monsterAttack() {
+            this.playerHealth -= randomAttack(
+                this.monsterAttack,
+                monsterAttackExtra
+            );
+            if (this.playerHealth < 0) {
+                this.playerHealth = 0;
+            }
+        },
     },
 });
 
