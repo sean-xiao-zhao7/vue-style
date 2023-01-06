e0039c07527c154a3ee82d1f3a0aafe3abe42b04
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-01-06 13:35:39 -0500

    Add base goals app

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const app = Vue.createApp({
     data() {
-        return { buttonChangeClasses: "" };
+        return { goals: [] };
     },
     methods: {
-        changeButtonStyle() {
-            if (this.buttonChangeClasses === "") {
-                this.buttonChangeClasses = "change1";
-            } else if (this.buttonChangeClasses === "change1") {
-                this.buttonChangeClasses = "change2";
-            } else {
-                this.buttonChangeClasses = "";
-            }
-            console.log(this.buttonChangeClasses);
+        addGoal(goal) {
+            this.goals.push(goal);
         },
     },
 });
