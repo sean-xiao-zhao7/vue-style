d9fe570c9d791581514c0b1af8db9a8e5ba4842f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-01-27 17:55:01 -0500

    Add register coach form

--- src/components/pages/coach/AddCoach.vue
+++ src/components/pages/coach/AddCoach.vue
@@ -1,3 +1,70 @@
+<script>
+export default {
+    data() {
+        return {
+            name: "",
+            phone: "",
+            email: "",
+            summary: "",
+            rate: null,
+            areas: [],
+        };
+    },
+    methods: {
+        submit() {},
+    },
+};
+</script>
+
 <template>
-    <div>Sign up as a coach</div>
+    <form @submit.prevent="submit">
+        <div id="form-container">
+            <input name="name" type="text" v-model="name" placeholder="Name" />
+            <input
+                name="phone"
+                type="text"
+                v-model="phone"
+                placeholder="Phone"
+            />
+            <input
+                name="email"
+                type="text"
+                v-model="email"
+                placeholder="Email"
+            />
+            <textarea
+                name="summary"
+                type="text"
+                v-model="summary"
+                placeholder="Summary"
+            ></textarea>
+            <input
+                name="rate"
+                type="number"
+                v-model="rate"
+                placeholder="Rate per hour"
+            />
+            <div>
+                Art
+                <input name="areas" type="checkbox" v-model="areas" />
+                Tech
+                <input name="areas" type="checkbox" v-model="areas" />
+                Craft
+                <input name="areas" type="checkbox" v-model="areas" />
+            </div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
 </template>
+
+<style scoped>
+#form-container {
+    display: grid;
+    grid-row-gap: 1rem;
+}
+textarea {
+    font-family: "Jost", sans-serif;
+    font-size: 1.4rem;
+    text-align: center;
+}
+</style>
